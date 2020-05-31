d2868938da07db37b704d0f8d8cef897202e5000
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-05-31 16:06:42 +0800

    Updates

---
+++ precache-manifest.31a05f2de73d4ba9b8b547e98225246d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3594cd83123bfd0caac64e7d8663751",
+    "url": "/covid-19-update-stg/index.html"
+  },
+  {
+    "revision": "80242a60d082f4206521",
+    "url": "/covid-19-update-stg/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0254d202cd3b30f648f2",
+    "url": "/covid-19-update-stg/static/css/main.06896f5c.chunk.css"
+  },
+  {
+    "revision": "80242a60d082f4206521",
+    "url": "/covid-19-update-stg/static/js/2.d2aed4ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-19-update-stg/static/js/2.d2aed4ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0254d202cd3b30f648f2",
+    "url": "/covid-19-update-stg/static/js/main.d4c39ef3.chunk.js"
+  },
+  {
+    "revision": "f508e9ef36e51377683f",
+    "url": "/covid-19-update-stg/static/js/runtime-main.6d722875.js"
+  }
+]);
