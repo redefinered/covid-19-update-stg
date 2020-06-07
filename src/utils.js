0cee224be9005afeeec71db370aa49ab8b3d8d7c
Red De Guzman <reddeguzman@MacBook-Pro.local>
2020-06-07 22:02:53 +0800

    added slug override for "United States"

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const countrySlugOverrides = [
+  { country: 'USA', slug: 'united-states' },
+  { country: 'United States', slug: 'united-states' },
+  { country: 'Hong Kong', slug: 'hong-kong-sar-china' }
+];
